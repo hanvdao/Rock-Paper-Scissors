136a0d18dd71343d39d70976cfe239705fc4fb06
Han Dao <hanvdao@gmail.com>
2021-04-06 19:48:27 -0500

    added GUI

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
   let randomInt = Math.floor(Math.random() * 3);
   switch (randomInt) {
@@ -13,30 +16,36 @@ function computerPlay() {
 function singleRound(playerSelection, computerSelection) {
   if (playerSelection.toUpperCase() == "ROCK") {
     if (computerSelection == "PAPER") {
-      return "You lose! Paper beats Rock!";
+      computerScore++;
+      return "You lose! Computer selects Paper!";
     }
     if (computerSelection == "ROCK") {
       return "It's a tie!";
     }
     if (computerSelection == "SCISSORS") {
-      return "You win! Rock beats Scissors!";
+      playerScore++;
+      return "You win! Computer selects Scissors!";
     }
   } else if (playerSelection.toUpperCase() == "PAPER") {
     if (computerSelection == "PAPER") {
       return "It's a tie!";
     }
     if (computerSelection == "ROCK") {
-      return "You win! Paper beats Rock";
+      playerScore++;
+      return "You win! Computer selects Rock";
     }
     if (computerSelection == "SCISSORS") {
-      return "You lose! Scissors beats Paper";
+      computerScore++;
+      return "You lose! Computer selects Scissors";
     }
   } else if (playerSelection.toUpperCase() == "SCISSORS") {
     if (computerSelection == "PAPER") {
-      return "You win! Scissors beats Paper";
+      playerScore++;
+      return "You win! Computer selects Paper";
     }
     if (computerSelection == "ROCK") {
-      return "You lose! Rock beats Scissors";
+      computerScore++;
+      return "You lose! Computer selects Rock";
     }
     if (computerSelection == "SCISSORS") {
       return "It's a tie!";
@@ -44,13 +53,38 @@ function singleRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let i = 0;
-  while (i < 5) {
-    const playerSelection = prompt("Enter your selection");
-    const computerSelection = computerPlay();
-    console.log(singleRound(playerSelection, computerSelection));
+const playerSelections = document.querySelector(".playerSelections");
+playerSelections.addEventListener("click", game);
+
+function game(event) {
+  if (playerScore == 5) {
+    alert("Game Over! You win the game!");
+    location.reload();
+  } else if (computerScore == 5) {
+    alert("Game Over! Computer wins the game!");
+    location.reload();
+  } else {
+    let playerSelection = "";
+
+    if (event.target.classList[0] != "playerSelections") {
+      playerSelection = event.target.classList[0];
+    } else {
+      return;
+    }
+    let computerSelection = computerPlay();
+
+    //showing message
+    let message = document.createElement("h3");
+    message.textContent = singleRound(playerSelection, computerSelection);
+    const messageDiv = document.querySelector(".message");
+    messageDiv.append("");
+    messageDiv.replaceChild(message, messageDiv.childNodes[0]);
+
+    //showing scores
+    const playerScoreDiv = document.querySelector(".playerScore");
+    const computerScoreDiv = document.querySelector(".computerScore");
+
+    playerScoreDiv.textContent = " " + playerScore;
+    computerScoreDiv.textContent = " " + computerScore;
   }
 }
-
-game();
